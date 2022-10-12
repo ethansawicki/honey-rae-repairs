2147766961ff0ce39c016c3e610a1c5accd2e8a5
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-10-12 02:46:56 -0500

    worked through 1-8 chapters

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,4 +1,103 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./tickets.css"
+
 export const TicketList = () => {
-    return <h2>List of Tickets</h2>
+    const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+    const [emergency, setEmergency] = useState(false)
+    const [openOnly, updateOpenOnly] = useState(false)
+    const navigate = useNavigate()
+
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
+
+    useEffect(
+        () => {
+            if(emergency) {
+                const emergencyTickets = tickets.filter(ticket => ticket.emergency === true)
+                setFiltered(emergencyTickets)
+            } else {
+                setFiltered(tickets)
+            }
+        },[emergency]
+    )
+
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                const response = await fetch(`http://localhost:8088/serviceTickets`)
+                const ticketArray = await response.json()
+                setTickets(ticketArray)
+            }
+            fetchData()
+        },
+        [] //When this array is empty, you are observing initial component state
+    )
+
+    useEffect(
+        () => {
+            if (honeyUserObject.staff) {
+                setFiltered(tickets)
+            } else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }   
+        },[tickets]
+    )
+
+    useEffect(
+        () => {
+            if (openOnly) {
+                const openTicketArray = tickets.filter(ticket => {
+                    return ticket.userId === honeyUserObject.id && ticket.dateCompleted === ""
+                })
+                setFiltered(openTicketArray)
+            } else {
+                const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+                setFiltered(myTickets)
+            }
+        }, [openOnly]
+    )
+
+    return <>
+    {
+        honeyUserObject.staff
+        ? <> 
+            <button
+                onClick={
+                    () => {
+                        setEmergency(true)
+                    }
+                }
+            >Emergency Only</button>
+            <button onClick={
+                () => {
+                    setEmergency(false)
+                }
+            }
+            >Show All</button>
+        </>
+        : <>
+        <button onClick={() => navigate("/ticket/create")}>Create Ticket</button>
+        <button onClick={() => updateOpenOnly(true)}>Open Tickets</button>
+        <button onClick={() => updateOpenOnly(false)}>Show All Tickets</button>
+        </>
+    }
+     <h2>List of Tickets</h2>
+    <article className="tickets">
+        {
+            filteredTickets.map(
+                (ticket) => {
+                    return <section className="ticket" key={ticket.id}>
+                        <header>{ticket.description}</header>
+                        <footer>Emergency: {ticket.emergency ? "❗" : "Nah Bro"}</footer>
+                    </section>
+                }
+            )
+        }
+    </article>
+    </>
+    
 }
 
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,7 +1,23 @@
+import { Outlet, Route, Routes } from "react-router-dom"
+import { TicketList } from "../tickets/TicketList"
+import { TicketForm } from "../tickets/TicketForm"
+
 export const ApplicationViews = () => {
-	return <>
-		<h1 className="title--main">Honey Rae Repairs</h1>
-		<div>Your one-stop shop for repairing your tech</div>
-	</>
+	return (
+		<Routes>
+			<Route path="/" element={
+				<>
+					<h1>Honey Rae Repair Shop</h1>
+					<div>Your one-stop-shop to get all your electronics fixed</div>
+
+					<Outlet />
+				</>
+			}>
+					<Route path="tickets" element={<TicketList />} />
+
+					<Route path="ticket/create" element={ <TicketForm /> } />
+				</Route>
+		</Routes>
+	)
 }
 
